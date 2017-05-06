057c581462b08ede8d4db159d37f7fd8b0fbfbb3
kimnorth <benignhumour@gmail.com>
2017-05-06 16:50:49 +0100

    Can add two numbers together.

---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,23 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
+
+  it('can add two numbers together', function(){
+    calculator.numberClick(1)
+    console.log(calculator.runningTotal)
+    calculator.operatorClick('+')
+    calculator.numberClick(4)
+    calculator.operatorClick('=')
+    assert.strictEqual(5, calculator.runningTotal)
+  })
+
+});
