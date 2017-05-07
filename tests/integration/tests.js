5d3cbef1109969bf73d8e6b3e8e0912425f172ad
kimnorth <benignhumour@gmail.com>
2017-05-07 16:49:25 +0100

    Can use with decimal numbers.

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -85,6 +85,18 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('-25')
   })    
 
+  it('should work with decimal numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('12.5')
+  })  
+
 });
 
 
